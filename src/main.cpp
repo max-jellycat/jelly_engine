6f7eacdf5e622d309d6503c1578513b78164cd17
max-jellycat <max@jellycat.fr>
2020-06-24 03:01:12 +0200

    Initial commit :wave:

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+int main()
+{
+  std::string output = "Hello World! 👋";
+  std::cout << output << std::endl;
+};
