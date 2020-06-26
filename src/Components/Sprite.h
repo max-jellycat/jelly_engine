1b282cc14b5b0b32b52bb50f0d3b1151267aa815
Maxime Blanc <max@jellycat.fr>
2020-06-27 00:04:42 +0200

    Moved animation logic from Sprite to Animator (#1)

--- src/Components/Sprite.h
+++ src/Components/Sprite.h
@@ -1,68 +1,35 @@
 #pragma once
 
 #include <SDL.h>
+#include <iostream>
 #include "Transform.h"
 #include "../TextureManager.h"
 #include "../Animation.h"
+#include "Animator.h"
 
 class Sprite : public Component {
 private:
     Transform *m_transform{};
     SDL_Texture *m_texture{};
+    Animator *m_animator;
     SDL_Rect m_sourceRect{};
     SDL_Rect m_destRect{};
     bool m_isAnimated;
-    int m_nFrames{};
-    int m_animationSpeed{};
     bool m_isFixed;
-    std::map<std::string, Animation> m_animations;
-    std::string m_currentAnimationName;
-    int m_animationIndex = 0;
 
 public:
     SDL_RendererFlip spriteFlip = SDL_FLIP_NONE;
 
-    explicit Sprite(const std::string& textureId) {
-        m_isAnimated = false;
+    explicit Sprite(const std::string& id) {
         m_isFixed = false;
-        SetTexture(textureId);
-    }
-
-    Sprite(const std::string& id, int nFrames, int animationSpeed, bool hasDirections, bool isFixed) {
-        m_isAnimated = true;
-        m_isFixed = isFixed;
-        m_nFrames = nFrames;
-        m_animationSpeed = animationSpeed;
-
-        if (hasDirections) {
-            Animation downAnimation(0, nFrames, animationSpeed);
-            Animation rightAnimation(1, nFrames, animationSpeed);
-            Animation leftAnimation(2, nFrames, animationSpeed);
-            Animation upAnimation(3, nFrames, animationSpeed);
-
-            m_animations.emplace("DownAnimation", downAnimation);
-            m_animations.emplace("RightAnimation", rightAnimation);
-            m_animations.emplace("LeftAnimation", leftAnimation);
-            m_animations.emplace("UpAnimation", upAnimation);
-
-            m_animationIndex = 0;
-            m_currentAnimationName = "DownAnimation";
-        } else {
-            Animation singleAnimation(0, nFrames, animationSpeed);
-            m_animations.emplace("SingleAnimation", singleAnimation);
-            m_animationIndex = 0;
-            m_currentAnimationName = "SingleAnimation";
-        }
-
-        Play(m_currentAnimationName);
+        m_isAnimated = false;
         SetTexture(id);
     }
 
-    void Play(const std::string& animationName) {
-        m_nFrames = m_animations[animationName].getFrames();
-        m_animationIndex = m_animations[animationName].getIndex();
-        m_animationSpeed = m_animations[animationName].getAnimationSpeed();
-        m_currentAnimationName = animationName;
+    Sprite(const std::string& id, bool isAnimated, bool isFixed) {
+        m_isFixed = isFixed;
+        m_isAnimated = isAnimated;
+        SetTexture(id);
     }
 
     void SetTexture(const std::string& assetTextureId) {
@@ -78,11 +45,11 @@ public:
     }
 
     void Update(float deltaTime) override {
-        if (m_isAnimated) {
-            m_sourceRect.x = m_sourceRect.w * static_cast<int>((SDL_GetTicks() / m_animationSpeed) % m_nFrames);
+        m_animator = owner->GetComponent<Animator>();
+        if (m_animator) {
+            m_sourceRect.x = m_sourceRect.w * static_cast<int>((SDL_GetTicks() / m_animator->getAnimationSpeed()) % m_animator->getAnimationFrames());
+            m_sourceRect.y = m_animator->getAnimationIndex() * m_transform->height;
         }
-
-        m_sourceRect.y = m_animationIndex * m_transform->height;
 
         m_destRect.x = static_cast<int>(m_transform->position.x);
         m_destRect.y = static_cast<int>(m_transform->position.y);
